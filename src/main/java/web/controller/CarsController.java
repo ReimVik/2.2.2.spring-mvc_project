a794e47b0a83e139efc0ad7551551c634fab18c3
ReimVik <99897049+ReimVik@users.noreply.github.com>
2022-06-06 10:29:48 +0300

    Add files via upload

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,18 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import web.dao.CarDao;
 
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
 
-    private final web.dao.carDAO carDAO;
+    private final CarDao carDAO;
 
     @Autowired
-    public CarsController(web.dao.carDAO carDAO) {
+    public CarsController(CarDao carDAO) {
         this.carDAO = carDAO;
     }
 
